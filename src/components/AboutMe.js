095c016988de60796eba0dad8edca5d7599495ac
jotish03 <jotishsharma03@gmail.com>
2023-08-17 20:30:58 +0530

    feat:project section added

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -45,7 +45,7 @@ const AboutMe = () => {
             My journey began with a curiosity for coding and design, leading me
             to create user-centric web applications that contribute to
             open-source projects. With an insatiable thirst for knowledge, I am
-            committed to pushing the boundaries of web technologies. Let&apos;s
+            committed to pushing the boundaries of web technologies. Lets
             collaborate and turn your digital vision into reality!
           </p>
         </div>
